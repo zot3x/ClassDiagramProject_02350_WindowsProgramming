b43a11f64fdd1e9faac242e8dd760dae77095ae4
Onepiece <Onepiece@ONEPIECEBCCC>
2016-11-08 09:05:44 +0100

    Rettet i UserControls og fået lavet sidePanelUsercontrol

--- 02350_ClassDiagram_Project/View/UserControls/Square_Usercontrol.xaml.cs
+++ 02350_ClassDiagram_Project/View/UserControls/Square_Usercontrol.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _02350_ClassDiagram_Project.View.UserControls
+namespace _02350_ClassDiagram_Project.UserControls
 {
     /// <summary>
     /// Interaction logic for Square_Usercontrol.xaml
